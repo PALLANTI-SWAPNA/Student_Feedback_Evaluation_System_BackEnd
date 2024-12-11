08fa47102343ec5ed62e1c9245997b7d4eaf4733
PALLANTI-SWAPNA <162856654+PALLANTI-SWAPNA@users.noreply.github.com>
2024-12-11 19:03:37 +0530

    Add files via upload

---
+++ src/main/java/com/project/FeedbackEvaluationSystem/FeedbackEvaluationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.FeedbackEvaluationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackEvaluationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackEvaluationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/FeedbackEvaluationSystem/FeedbackEvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FeedbackEvaluationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackEvaluationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
